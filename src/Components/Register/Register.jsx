5f6777e1aee291041f265c62d6025459e55359cb
edersonn31 <ar882022@gmail.com>
2025-02-01 06:41:46 +0200

    the max length of password and the error message updated

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -27,11 +27,11 @@ export default function Register() {
   let validationSchema = Yup.object().shape({
     name: Yup.string()
       .min(3, "min length is 3")
-      .max(9, "max length is 9")
+      .max(12, "max length is 12")
       .required(),
     email: Yup.string().email("invalid email").required(),
     password: Yup.string()
-      .matches(/^[A-Z]\w{5,10}$/, "password invalid ex(ahmed123)")
+      .matches(/^[A-Z]\w{5,20}$/, "password invalid ex(Ahmed123)")
       .required(),
     rePassword: Yup.string()
       .oneOf([Yup.ref("password")], "password not match ")
@@ -162,7 +162,7 @@ export default function Register() {
                 htmlFor="rePassword"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
-                Enter Your rePassword
+                comfirm password
               </label>
             </div>
             {formik.errors.rePassword && formik.touched.rePassword && (
